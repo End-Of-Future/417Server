e1d5465b405c855ee3f7ecff1158777ab3373fbb
F-Ate <118088480+End-Of-Future@users.noreply.github.com>
2023-01-07 13:28:58 +0800

    Delete server directory

--- server/data-manager/entities/CreateAndUpdateTimeBase.ts
+++
@@ -1,9 +0,0 @@
-import {CreateDateColumn, UpdateDateColumn} from "typeorm";
-
-export abstract class CreateAndUpdateTimeBase {
-    @CreateDateColumn()
-    createTime: Date;
-
-    @UpdateDateColumn()
-    updateTime: Date;
-}
